c0c6ac563bfa376710f5be61ade3c6e146e63bb0
amogh <amogh1773@gmail.com>
2025-07-11 10:18:22 +0530

    switch_theme now accepts user input to set light/dark theme

--- readme.md
+++ readme.md
@@ -23,4 +23,7 @@ alias kitty_theme_random="/home/amogh/.config/kitty/random_theme.sh"
 | `kitty_theme_random`                | random_theme.sh | Set a random theme                                                                                 |
 | `kitty_theme_random <str>`          |                 | Set a theme that has the given keyword somewhere in its name                                       |
 | [Example] `kitty_theme_random bela` |                 | Can set one of `Belafonte_Day`, `Belafonte_Night` or any other matches containing 'bela' at random |
+| <hr />                              | <hr />          | <hr />                                                                                             |
 | `kitty_theme_set`                   | switch_theme.sh | Sets a theme from a bunch of predefined themes depending on light/dark mode                        |
+| [Example] `kitty_theme_set light`   |                 | Sets one of the light themes (from predefined themes)                                              |
+| [Example] `kitty_theme_set dark`    |                 | Sets one of the dark themes (from predefined themes)                                               |
--- switch_theme.sh
+++ switch_theme.sh
@@ -3,22 +3,27 @@
 THEME_DIR=~/.config/kitty/themes
 
 DARK_THEMES=(Obsidian.conf Dracula.conf Monokai.conf gruvbox_dark.conf WildCherry.conf Monokai_Soda.conf)
-LIGHT_THEMES=(Solarized_Light.conf Belafonte_Day.conf ayu_light.conf)
+LIGHT_THEMES=(Solarized_Light.conf Belafonte_Day.conf ayu_light.conf Github.conf)
 
 get_random_theme() {
     local themes=("$@")
-    echo "${themes[$RANDOM % ${#themes[@]}]}"
+    echo "${themes[RANDOM % ${#themes[@]}]}"
 }
 
-COLOR_SCHEME=$(gsettings get org.gnome.desktop.interface color-scheme)
-
-if [[ "$COLOR_SCHEME" == "'prefer-dark'" ]]; then
-    CHOSEN_DARK_THEME=$(get_random_theme "${DARK_THEMES[@]}")
-    kitty @ set-colors -a "$THEME_DIR/$CHOSEN_DARK_THEME"
-    echo "Applied theme: ${CHOSEN_DARK_THEME%?????}"
+if [[ "$1" == "dark" || "$1" == "light" ]]; then
+    MODE="$1"
 else
-    CHOSEN_LIGHT_THEME=$(get_random_theme "${LIGHT_THEMES[@]}")
-    kitty @ set-colors -a "$THEME_DIR/$CHOSEN_LIGHT_THEME"
-    # echo "Applied theme: $(basename "$CHOSEN_LIGHT_THEME")"
-    echo "Applied theme: ${CHOSEN_LIGHT_THEME%?????}"
+    COLOR_SCHEME=$(gsettings get org.gnome.desktop.interface color-scheme)
+    MODE=$([[ "$COLOR_SCHEME" == "'prefer-dark'" ]] && echo "dark" || echo "light")
 fi
+
+# Use name reference to select array
+if [[ "$MODE" == "dark" ]]; then
+    declare -n THEMES_REF=DARK_THEMES
+else
+    declare -n THEMES_REF=LIGHT_THEMES
+fi
+
+CHOSEN_THEME=$(get_random_theme "${THEMES_REF[@]}")
+kitty @ set-colors -a "$THEME_DIR/$CHOSEN_THEME"
+echo "Applied $MODE theme: ${CHOSEN_THEME%.conf}"
